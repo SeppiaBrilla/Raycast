ca2110a20c4f0830d371fc81431a7616951a4839
Alessio Pellegrino <seppiabrilla@pop-os.localdomain>
2020-08-11 02:19:31 +0200

    ora va

--- Raycast.py
+++ Raycast.py
@@ -66,6 +66,7 @@ class Muro:
 
 
 def check(raggi, muri, screen, color):
+    scene = [None] * len(raggi)
     for r in raggi:
         minimum = (100000, 100000)
         for muro in muri:
@@ -73,7 +74,9 @@ def check(raggi, muri, screen, color):
             pt = r.collision(muro)
             if not pt is None and distance(minimum, (r.xInizio, r.yInizio)) > distance(pt, (r.xInizio, r.yInizio)):
                 minimum = pt
+                scene[raggi.index(r)] = distance(pt, (r.xInizio, r.yInizio))
         r.setTo(minimum)
+    return scene
             
 def distance(p0, p1):
     return math.sqrt((p0[0] - p1[0])**2 + (p0[1] - p1[1])**2)
@@ -83,16 +86,33 @@ def move(x, y, raggi):
     for r in raggi:
         r.setInit(x, y)
 
+def map(n, start1, stop1, start2, stop2):
+  return (n - start1) / (stop1 - start1) *(stop2 - start2) + start2
+
+def CreateRaggi(min, max, screen, color):
+    raggi = []
+    for a in range(min, max):
+        xInizio = 400
+        yInizio = 400
+        xDir= math.cos(math.radians(a))
+        yDir = math.sin(math.radians(a))
+
+        r = linea(xInizio, yInizio, xDir, yDir, screen, color, 1, (0, 0))
+        raggi.append(r)
+    return raggi
 
 
 def Main():
     pygame.init()
     
-    size = width, height = 800, 800
-    centerX = int(width/2)
+    size = width, height = 1600, 800
+    centerX = int(width/4)
     centerY = int(height/2)
+    sceneW = width / 2
     x = centerX
     y = centerY
+    min = 0
+    max = 45
     font = pygame.font.SysFont("freesansbold.ttf", 50)
     screen = pygame.display.set_mode(size)
     black = 0, 0, 0
@@ -104,7 +124,7 @@ def Main():
 
 
     for i in range(0, 10):
-        xInizio = random.randint(0, width - 150)
+        xInizio = random.randint(0, sceneW - 150)
         yInizio = random.randint(0, height - 150)
         xFine = xInizio + random.randint(0, 150)  
         yFine = yInizio + random.randint(0, 150)
@@ -113,18 +133,11 @@ def Main():
         muri.append(muro)
 
     muri.append(Muro(0, 0, 0, height, screen, red, 5))
-    muri.append(Muro(0, 0, width, 0, screen, red, 5))
-    muri.append(Muro(width, 0, width, height, screen, red, 5))
-    muri.append(Muro(0, height, width, height, screen, red, 5))
+    muri.append(Muro(0, 0, sceneW, 0, screen, red, 5))
+    muri.append(Muro(sceneW, 0, sceneW, height, screen, red, 5))
+    muri.append(Muro(0, height, sceneW, height, screen, red, 5))
 
-    for a in range(0, 360):
-        xInizio = x
-        yInizio = y
-        xDir= math.cos(a)
-        yDir = math.sin(a)
-
-        r = linea(xInizio, yInizio, xDir, yDir, screen, white, 1, (0, 0))
-        raggi.append(r)
+    raggi = CreateRaggi(min, max,screen,white)
 
 
 
@@ -133,8 +146,26 @@ def Main():
             if event.type == pygame.QUIT: 
                 sys.exit()
 
+            if event.type == pygame.KEYDOWN:
+
+                if event.key == pygame.K_UP:
+                    max += 1
+                    raggi = CreateRaggi(min,max,screen,white)
+                elif event.key == pygame.K_DOWN:
+                    max -= 1
+                    raggi = CreateRaggi(min,max,screen,white)
+                elif event.key == pygame.K_d:
+                    max += 1
+                    min += 1 
+                    raggi = CreateRaggi(min,max,screen,white)
+                elif event.key == pygame.K_a:
+                    max -= 1
+                    min -= 1
+                    raggi = CreateRaggi(min,max,screen,white)
+
             x, y = pygame.mouse.get_pos()
-            move(x, y, raggi)
+            if x < sceneW:
+                move(x, y, raggi)
             
         screen.fill(black)
         for r in raggi:
@@ -143,7 +174,18 @@ def Main():
         for muro in muri:
             muro.draw()
             
-        check(raggi, muri, screen, white)
+        scene = check(raggi, muri, screen, white)
+        w = sceneW / len(scene)
+        for s in scene:
+            if s:
+                sq = s * s
+                wSq = sceneW * sceneW
+                b = map(sq, 0, wSq, 255, 0)
+                print(b)
+                h = map(s, 0, sceneW, 0, 800)
+                if b > 0:
+                    pygame.draw.rect(screen,(b, b, b),(sceneW + scene.index(s) * w + w, 800,w + 1, - (800 - h)))
+
         pygame.display.flip()
 
 
